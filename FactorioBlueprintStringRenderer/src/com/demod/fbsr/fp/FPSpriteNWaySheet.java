a30f47c0eb9a53bcbf886b2cdb0cdb7baa4df25c
Demod <demodulent@gmail.com>
2025-01-19 23:25:18 -0500

    Fixed if frames < dir count

--- FactorioBlueprintStringRenderer/src/com/demod/fbsr/fp/FPSpriteNWaySheet.java
+++ FactorioBlueprintStringRenderer/src/com/demod/fbsr/fp/FPSpriteNWaySheet.java
@@ -10,29 +10,31 @@ import com.demod.fbsr.Sprite;
 
 public class FPSpriteNWaySheet extends FPSpriteParameters {
 
-	public static Optional<FPSpriteNWaySheet> opt(LuaValue lua, int defaultFrames) {
+	public static Optional<FPSpriteNWaySheet> opt(LuaValue lua, int directionCount) {
 		if (lua.isnil()) {
 			return Optional.empty();
 		}
-		return Optional.of(new FPSpriteNWaySheet(lua, defaultFrames));
+		return Optional.of(new FPSpriteNWaySheet(lua, directionCount));
 	}
 
 	public final int frames;
 
-	public FPSpriteNWaySheet(LuaValue lua, int defaultFrames) {
-		super(lua);
+	private final int directionCount;
 
-		frames = lua.get("frames").optint(defaultFrames);
+	public FPSpriteNWaySheet(LuaValue lua, int directionCount) {
+		super(lua);
+		this.directionCount = directionCount;
+
+		frames = lua.get("frames").optint(directionCount);
 	}
 
 	public Sprite createSprite(Direction direction) {
 		int x = this.x;
 
-		// TODO what do I do when it is not 4 or 8?
-		if (frames == 4) {
-			x += width * direction.cardinal();
-		} else if (frames == 8) {
-			x += width * direction.ordinal();
+		if (directionCount == 4) {
+			x += width * (direction.cardinal() % frames);
+		} else if (directionCount == 8) {
+			x += width * (direction.ordinal() % frames);
 		}
 
 		return RenderUtils.createSprite(filename.get(), drawAsShadow, blendMode, getEffectiveTint(), x, y, width,
