b1382903d54dbd5e3d222219097913ea106b53f1
Demod <demodulent@gmail.com>
2025-02-05 08:52:18 -0500

    changed key check to null check

--- FactorioBlueprintStringRenderer/src/com/demod/fbsr/BSUtils.java
+++ FactorioBlueprintStringRenderer/src/com/demod/fbsr/BSUtils.java
@@ -16,14 +16,14 @@ import com.google.common.collect.ImmutableList.Builder;
 
 public final class BSUtils {
 	public static Direction direction(JSONObject json, String key) {
-		if (!json.has(key)) {
+		if (json.isNull(key)) {
 			return Direction.NORTH;
 		}
 		return Direction.values()[json.getInt(key) / 2];
 	}
 
 	public static <T> List<T> list(JSONObject json, String key, Function<JSONObject, T> factory) {
-		if (!json.has(key)) {
+		if (json.isNull(key)) {
 			return ImmutableList.of();
 		}
 		Builder<T> builder = ImmutableList.builder();
@@ -32,7 +32,7 @@ public final class BSUtils {
 	}
 
 	public static List<Integer> listInt(JSONObject json, String key) {
-		if (!json.has(key)) {
+		if (json.isNull(key)) {
 			return ImmutableList.of();
 		}
 		Builder<Integer> builder = ImmutableList.builder();
@@ -41,42 +41,42 @@ public final class BSUtils {
 	}
 
 	public static <T> Optional<T> opt(JSONObject json, String key, Function<JSONObject, T> factory) {
-		if (!json.has(key)) {
+		if (json.isNull(key)) {
 			return Optional.empty();
 		}
 		return Optional.of(factory.apply(json.getJSONObject(key)));
 	}
 
 	public static Optional<Boolean> optBool(JSONObject json, String key) {
-		if (!json.has(key)) {
+		if (json.isNull(key)) {
 			return Optional.empty();
 		}
 		return Optional.of(json.getBoolean(key));
 	}
 
 	public static OptionalDouble optDouble(JSONObject json, String key) {
-		if (!json.has(key)) {
+		if (json.isNull(key)) {
 			return OptionalDouble.empty();
 		}
 		return OptionalDouble.of(json.getDouble(key));
 	}
 
 	public static OptionalInt optInt(JSONObject json, String key) {
-		if (!json.has(key)) {
+		if (json.isNull(key)) {
 			return OptionalInt.empty();
 		}
 		return OptionalInt.of(json.getInt(key));
 	}
 
 	public static <T> Optional<T> optKeyed(JSONObject json, String key, BiFunction<JSONObject, String, T> factory) {
-		if (!json.has(key)) {
+		if (json.isNull(key)) {
 			return Optional.empty();
 		}
 		return Optional.of(factory.apply(json, key));
 	}
 
 	public static <T> Optional<List<T>> optList(JSONObject json, String key, Function<JSONObject, T> factory) {
-		if (!json.has(key)) {
+		if (json.isNull(key)) {
 			return Optional.empty();
 		}
 		Builder<T> builder = ImmutableList.builder();
@@ -85,7 +85,7 @@ public final class BSUtils {
 	}
 
 	public static Optional<BSPosition> optPosition(JSONObject json, String key) {
-		if (!json.has(key)) {
+		if (json.isNull(key)) {
 			return Optional.empty();
 		}
 		return Optional.of(BSPosition.parse(json.get(key)));
@@ -96,7 +96,7 @@ public final class BSUtils {
 	}
 
 	public static Optional<String> optString(JSONObject json, String key) {
-		if (!json.has(key)) {
+		if (json.isNull(key)) {
 			return Optional.empty();
 		}
 		return Optional.of(json.getString(key));
