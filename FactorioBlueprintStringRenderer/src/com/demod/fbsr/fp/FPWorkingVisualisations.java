73512dadb010a3449f0105b122ead93d5d987ea7
Demod <demodulent@gmail.com>
2025-05-05 15:51:20 -0400

    unfix the bug workaround

--- FactorioBlueprintStringRenderer/src/com/demod/fbsr/fp/FPWorkingVisualisations.java
+++ FactorioBlueprintStringRenderer/src/com/demod/fbsr/fp/FPWorkingVisualisations.java
@@ -17,54 +17,25 @@ import com.google.common.collect.ImmutableList;
 public class FPWorkingVisualisations {
 	public final Optional<FPAnimation4Way> animation;
 	public final Optional<FPAnimation4Way> idleAnimation;
-	public final Optional<List<Optional<FPWorkingVisualisation>>> workingVisualisations;
+	public final Optional<List<FPWorkingVisualisation>> workingVisualisations;
 
 	public final Optional<FPSprite4Way> integrationPatch;
 
 	public FPWorkingVisualisations(LuaValue lua) {
 		idleAnimation = FPUtils.opt(lua.get("idle_animation"), FPAnimation4Way::new);
 		animation = FPUtils.opt(lua.get("animation"), FPAnimation4Way::new).or(() -> idleAnimation);
-
-		// TODO there are bugs in factorio about this structure
-		LuaValue luaWorkingVisualisations = lua.get("working_visualisations");
-		if (checkBugInWorkingVisualisations(luaWorkingVisualisations)) {
-			workingVisualisations = Optional.empty();
-		} else {
-			workingVisualisations = Optional.of(
-					ImmutableList.of(Optional.of(new FPWorkingVisualisation(luaWorkingVisualisations.get("layer")))));
-		}
+		workingVisualisations = FPUtils.optList(lua.get("working_visualisations"), FPWorkingVisualisation::new);
 
 		// Not mentioned in docs, found in py
 		integrationPatch = FPUtils.opt(lua.get("integration_patch"), FPSprite4Way::new);
 	}
 
-	private boolean checkBugInWorkingVisualisations(LuaValue lua) {
-		if (lua.isnil()) {
-			return false;
-		}
-		if (lua.isarray()) {
-			return false;
-		}
-		// Detect any keys that are not integers
-		return ((JSONObject) lua.getJson()).keySet().stream().anyMatch(s -> {
-			try {
-				Integer.parseInt(s);
-			} catch (NumberFormatException e) {
-				return true;
-			}
-			return false;
-		});
-	}
-
 	public void defineSprites(Consumer<? super SpriteDef> consumer, Direction direction, int frame) {
 		animation.ifPresent(animation -> animation.defineSprites(consumer, direction, frame));
 		workingVisualisations.ifPresent(list -> {
-			for (Optional<FPWorkingVisualisation> workingVisualisation : list) {
-				if (!workingVisualisation.isPresent()) {
-					continue;
-				}
-				if (workingVisualisation.get().alwaysDraw) {
-					workingVisualisation.get().defineSprites(consumer, direction, frame);
+			for (FPWorkingVisualisation workingVisualisation : list) {
+				if (workingVisualisation.alwaysDraw) {
+					workingVisualisation.defineSprites(consumer, direction, frame);
 				}
 			}
 		});
