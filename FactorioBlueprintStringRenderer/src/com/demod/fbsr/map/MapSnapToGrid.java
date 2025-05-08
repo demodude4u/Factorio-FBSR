3354620d060c17370b1b902ade48f48791cd1a84
Demod <demodulent@gmail.com>
2025-05-07 23:22:45 -0400

    cleaner dashes

--- FactorioBlueprintStringRenderer/src/com/demod/fbsr/map/MapSnapToGrid.java
+++ FactorioBlueprintStringRenderer/src/com/demod/fbsr/map/MapSnapToGrid.java
@@ -3,7 +3,7 @@ package com.demod.fbsr.map;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.geom.RoundRectangle2D;
+import java.awt.geom.Rectangle2D;
 
 import com.demod.fbsr.Layer;
 
@@ -12,10 +12,10 @@ public class MapSnapToGrid extends MapRenderable implements MapBounded {
     private static final Color GRID_COLOR1 = Color.GREEN.darker().darker();
     private static final Color GRID_COLOR2 = Color.GREEN.darker();
     private static final BasicStroke GRID_STROKE1 = new BasicStroke(
-        0.25f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 1.0f, new float[] {0.6f, 0.4f}, 0.0f
+        0.25f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 1.0f, new float[] {0.6f, 0.4f}, 0.8f
     );
     private static final BasicStroke GRID_STROKE2 = new BasicStroke(
-        0.19f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 1.0f, new float[] {0.57f, 0.43f}, 0.0f
+        0.19f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 1.0f, new float[] {0.57f, 0.43f}, 0.785f
     );
 
     private MapRect bounds;
@@ -28,9 +28,7 @@ public class MapSnapToGrid extends MapRenderable implements MapBounded {
 
     @Override
     public void render(Graphics2D g) {
-        RoundRectangle2D.Double rect = new RoundRectangle2D.Double(
-            bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight(), 0.25, 0.25
-        );
+        Rectangle2D.Double rect = new Rectangle2D.Double(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight());
         g.setColor(GRID_COLOR1);
         g.setStroke(GRID_STROKE1);
         g.draw(rect);
